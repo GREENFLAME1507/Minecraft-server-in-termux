7550512ffb524f5da76771df8450b426b1382b93
GREENFLAME1507 <160883635+GREENFLAME1507@users.noreply.github.com>
2024-05-02 10:34:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,3 +9,43 @@ clear
 echo "Run the following command within ubuntu install"
 echo "wget https://raw.githubusercontent.com/GREENFLAME1507/Minecraft-server-in-termux/packages.sh"
 exit 
+
+
+
+
+
+#!/bin/bash
+
+# Prompt the user to choose between Option 1 and Option 2
+echo "Please choose an option:"
+echo "1. Install recommended os"
+echo "2. Don't Install os"
+read -p "Enter your choice (1 or 2): " choice
+
+# Check the user's choice and execute the corresponding script
+case $choice in
+    1)
+        echo "Innstalling ubuntu......."
+        wget https://raw.githubusercontent.com/GREENFLAME1507/Minecraft-server-in-termux/os/ubuntu.sh
+        chmod +x ubuntu.sh
+        ./ubuntu.sh
+        clear 
+        echo "Run the following command within ubuntu install"
+        echo "wget https://raw.githubusercontent.com/GREENFLAME1507/Minecraft-server-in-termux/packages.sh"
+        echo "and run "./packages.sh""
+        exit 
+        ;;
+    2)
+        echo "It is recommende to install the provided os !"
+        echo "anyway moving on "
+        wget https://raw.githubusercontent.com/GREENFLAME1507/Minecraft-server-in-termux/packages.sh
+        chmod +x packages.sh
+        ./packages.sh
+        ;;
+    *)
+        echo "Invalid choice. Please enter 1 or 2."
+        ;;
+esac
+
+# Exit the running script
+exit
