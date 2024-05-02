1a4ef4ec3a0d81f764fec234d782fae0c2da568a
GREENFLAME1507 <160883635+GREENFLAME1507@users.noreply.github.com>
2024-05-02 11:40:39 +0530

    Update Bedrock-server.sh

--- server/Bedrock-server.sh
+++ server/Bedrock-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+cd
 apt install unzip -y
 adduser minecraft-runner
+su minecraft-runner
 mkdir /home/minecraft-runner/bedrock-server
 cd /home/minecraft-runner/bedrock-server
 clear
@@ -15,7 +17,30 @@ echo "EDIT THIS FILE THE WAY YOU LIKE IT AND PRESS CTRL+O TO SAVE & CTRL+X TO EX
 read -rs -t3 -n1 "Press any key or wait to continue ..."
 nano server.properties
 clear
-echo "TO START IN FUTURE USELD_LIBRARY_PATH=. ./bedrock_server"
-echo "START MINECRAFT SERVER ????"
 read -rs -t3 -n1 "Press any key or wait to continue ..."
 LD_LIBRARY_PATH=. ./bedrock_server
+/stop
+
+
+
+source_file="server/bedrock-dependencies/bedrock.service"
+destination_dir="/etc/systemd/system/"
+
+# Check if the destination directory exists, if not create it
+mkdir -p "$destination_dir"
+
+# Copy the file to the destination directory
+cp "$source_file" "$destination_dir"
+
+# Notify the user that the file has been copied
+echo "File '$source_file' copied to '$destination_dir'"
+
+service bedrock.service enable
+service bedrock start
+
+clear 
+
+service bedrock status
+
+
+echo "TO START SERVER USE "LD_LIBRARY_PATH=. ./bedrock_server""
