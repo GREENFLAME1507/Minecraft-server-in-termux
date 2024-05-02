70c2475edf67827f9fa195a97692092db26b11dd
GREENFLAME1507 <160883635+GREENFLAME1507@users.noreply.github.com>
2024-05-02 10:22:45 +0530

    Update java-version-selector.sh

--- server/ver/java-version-selector.sh
+++ server/ver/java-version-selector.sh
@@ -2,22 +2,27 @@
 #!/bin/bash
 
 # Prompt the user to choose between Option 1 and Option 2
-echo "Please choose an option:"
-echo "1. Option 1"
-echo "2. Option 2"
+echo "select the version you wnt to install :"
+echo "1. 1.20.4"
+echo "2. 1.20.5"
 read -p "Enter your choice (1 or 2): " choice
 
 # Check the user's choice and execute the corresponding script
 case $choice in
     1)
-        echo "You chose Option 1"
-        # Call script1.sh for Option 1
-        ./script1.sh
+        echo "installing 1.20.4......."
+        wget "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
+        java -jar server.jar
+        clear
+        echo "run the following command to start the server"
+        echo "java -Xmx1G -Xms3G -jar minecraft_server.1.20.4.jar nogui"
         ;;
     2)
-        echo "You chose Option 2"
-        # Call script2.sh for Option 2
-        ./script2.sh
+        echo "installing 1.20.5........"
+        wget "https://piston-data.mojang.com/v1/objects/79493072f65e17243fd36a699c9a96b4381feb91/server.jar"
+        java -jar server.jar
+        echo "run the following command to start the server"
+        echo "java -Xmx1G -Xms3G -jar minecraft_server.1.20.5.jar nogui"
         ;;
     *)
         echo "Invalid choice. Please enter 1 or 2."
